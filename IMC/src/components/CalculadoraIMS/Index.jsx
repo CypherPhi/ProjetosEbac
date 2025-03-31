9c3f6cd5dcf125e2dc9f5c24c1885766967b501e
CypherPhi <rodriguesvieira1911@gmail.com>
2025-03-31 10:47:14 -0300

    ajuste da informacao

--- IMC/src/components/CalculadoraIMS/Index.jsx
+++ IMC/src/components/CalculadoraIMS/Index.jsx
@@ -6,6 +6,7 @@ const CalculadoraIMS = () => {
   let [peso, setPeso] = useState(0);
   let [altura, setAltura] = useState(0);
   let [imc, setImc] = useState(0);
+  let [tipo, setTipo] = useState(" ");
   let [resultado, setResultado] = useState(false);
 
   const calcularIMC = (e) => {
@@ -16,6 +17,21 @@ const CalculadoraIMS = () => {
     let imcFormatado = imc.toFixed(2);
     setImc(imcFormatado);
     setResultado(true);
+  };
+
+  const alterar = () => {
+    if (imc < 18.5) {
+      setTipo("Baixo do peso");
+    } else if (imc < 24.9) {
+      setTipo("Normal");
+    } else if (imc < 29.9) {
+      setTipo("Sobrepeso");
+    } else if (imc < 34.9) {
+      console.log("foi");
+      setTipo("Obesidade");
+    } else if (imc < 35) {
+      setTipo("Obesidade Severa");
+    }
   };
 
   return (
@@ -44,12 +60,17 @@ const CalculadoraIMS = () => {
             onChange={(e) => setPeso(parseFloat(e.target.value))}
           />
         </div>
-        <button type="submit" className={styles.calculadora__bloco__button}>
+        <button
+          type="submit"
+          className={styles.calculadora__bloco__button}
+          onClick={alterar}
+        >
           Calcular
         </button>
         <div>
           <h2 className={styles.calculadora__bloco__text}>Seu IMC é</h2>
           <p className={styles.calculadora__bloco__imc}>{imc}</p>
+          <p className={styles.calculadora__bloco__imc}>{tipo}</p>
         </div>
       </form>
       {resultado ? (
