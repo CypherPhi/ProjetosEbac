eddbf4a3b121669d49f79c931e39c2302d27db35
CypherPhi <rodriguesvieira1911@gmail.com>
2025-05-08 10:54:47 -0300

    ajuste com react-redux

---
+++ ebac_sports/src/containers/Produtos.tsx
@@ -0,0 +1,38 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../store'
+
+import { Produto as ProdutoType } from '../App'
+import Produto from '../components/Produto'
+
+import * as S from './styles'
+
+type Props = {
+  produtos: ProdutoType[]
+}
+
+const ProdutosComponent = ({ produtos }: Props) => {
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
+
+  const produtoEstaNosFavoritos = (produto: ProdutoType) => {
+    const produtoId = produto.id
+    const IdsDosFavoritos = favoritos.map((f) => f.id)
+
+    return IdsDosFavoritos.includes(produtoId)
+  }
+
+  return (
+    <>
+      <S.Produtos>
+        {produtos.map((produto) => (
+          <Produto
+            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+            key={produto.id}
+            produto={produto}
+          />
+        ))}
+      </S.Produtos>
+    </>
+  )
+}
+
+export default ProdutosComponent
---
+++ ebac_sports/src/store/reducers/favoritos.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type produtoState = {
+  itens: Produto[]
+}
+
+const initialState: produtoState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado aos favoritos')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
